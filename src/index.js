bedb64b9a64ff5bcb849d017c6f05eeabaf97f13
Winter.Ji-季天冬 <jitd1222@gmail.com>
2023-04-07 19:18:20 +0800

    feat: Create basic Express app

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-// index.js
-// This is the main entry point of our application
+const express = require('express')
+const app = express()
+const port = process.env.port || 4000
+
+app.get('/', (req, res) => res.send('Hello, world!!!'))
+
+app.listen(port, () => console.log(`Server running at http://localhost:${port}`))
